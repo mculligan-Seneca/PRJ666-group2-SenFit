fbd6f5db274b32e93a37f92a1f8d24f29bda68a0
mculligan-Seneca <44836281+mculligan-Seneca@users.noreply.github.com>
2021-01-24 21:18:25 -0500

    begun DAO

---
+++ SenFit/app/src/main/java/com/example/senfit/MemberDAO.java
@@ -0,0 +1,15 @@
+/*
+PRJ666 Sen-Fit
+init date: January 24th 2021
+Author Mitchell Culligan
+Version 1.0
+MemberDAO class
+This class repersents the data access object of the member class
+ */
+package com.example.senfit;
+
+public abstract  class MemberDAO {
+
+
+
+}
--- SenFit/app/src/main/java/com/example/senfit/OnlineClass.java
+++ SenFit/app/src/main/java/com/example/senfit/OnlineClass.java
@@ -1,4 +1,38 @@
+/*
+PRJ666 Sen-Fit
+init date: January 24th 2021
+Author Mitchell Culligan
+Version 1.0
+OnlineClass class
+This class repersents the data to be stored for an online class entity.
+ */
 package com.example.senfit;
 
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+
+@Entity(tableName="onlineClasses")
 public class OnlineClass {
+
+    @PrimaryKey(autoGenerate=true)
+    private int onlineClassId;
+
+    @ColumnInfo(name="class_date")
+    private Date classDate;
+
+    @ColumnInfo(name="start_time")
+    private Timestamp startTime;
+
+    @ColumnInfo(name="end_time")
+    private Timestamp endTime;
+
+    @ColumnInfo(name="trainerId")
+    public int trainerId;//TODO:ADD foreign key associations
+
+        //TODO: add bridge tables
 }
