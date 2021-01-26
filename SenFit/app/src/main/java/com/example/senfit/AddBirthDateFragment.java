802f803323cc670271ea848009db197d6ec59257
mculligan-Seneca <44836281+mculligan-Seneca@users.noreply.github.com>
2021-01-26 15:54:32 -0500

    colours and datepicker added

---
+++ SenFit/app/src/main/java/com/example/senfit/AddBirthDateFragment.java
@@ -0,0 +1,69 @@
+package com.example.senfit;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ PRJ666 Sen-Fit
+ init date: January 26th 2021
+ Author Mitchell Culligan
+ Version 1.0
+ AddBirthDateFragment Class
+ This fragment class extends the dialog fragment class and will provide the user with a seperate dialog window to
+ select their birthdate. This fragment will be used in the sign up process
+ */
+public class AddBirthDateFragment extends DialogFragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public AddBirthDateFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment AddBirthDateFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static AddBirthDateFragment newInstance(String param1, String param2) {
+        AddBirthDateFragment fragment = new AddBirthDateFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_birth_date, container, false);
+    }
+}
