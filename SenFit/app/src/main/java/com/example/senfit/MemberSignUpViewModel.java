dbf0d8f5d342a2695b3d1da60d0a26a657d553b9
mculligan-Seneca <44836281+mculligan-Seneca@users.noreply.github.com>
2021-01-28 18:55:52 -0500

    added xml files

--- SenFit/app/src/main/java/com/example/senfit/MemberSignUpViewModel.java
+++ SenFit/app/src/main/java/com/example/senfit/MemberSignUpViewModel.java
@@ -1,4 +1,11 @@
-
+/*
+PRJ666 Sen-Fit
+init date: January 28th 2021
+Author Mitchell Culligan
+Version 1.0
+MemberSignUpViewModel
+This viewmodel class is meant to store the member data for the sign up use case
+ */
 package com.example.senfit;
 
 import androidx.lifecycle.ViewModel;
