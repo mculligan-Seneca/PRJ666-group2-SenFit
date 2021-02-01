11d6f812b088d80f644edd544d29bcd94618e064
mculligan-Seneca <44836281+mculligan-Seneca@users.noreply.github.com>
2021-02-01 13:11:53 -0500

    covid survey activity added

--- SenFit/app/src/main/java/com/example/senfit/CovidSurveyActivity.java
+++ SenFit/app/src/main/java/com/example/senfit/CovidSurveyActivity.java
@@ -1,21 +1,22 @@
+/*
+PRJ666 Sen-Fit
+init date: Febuary 1st 2021
+Author Mitchell Culligan
+Version 1.0
+Covid survey activity
+
+ */
 package com.example.senfit;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
-import android.support.wearable.activity.WearableActivity;
-import android.widget.TextView;
 
-public class CovidSurveyActivity extends WearableActivity {
-
-    private TextView mTextView;
+public class CovidSurveyActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_covid_survey);
-
-        mTextView = (TextView) findViewById(R.id.text);
-
-        // Enables Always-on
-        setAmbientEnabled();
     }
 }
