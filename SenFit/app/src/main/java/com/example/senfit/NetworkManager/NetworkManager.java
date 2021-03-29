87ce279fa2f233be5584e984fc3e75ba53b8903b
mculligan-Seneca <44836281+mculligan-Seneca@users.noreply.github.com>
2021-03-29 13:16:08 -0400

    networkManager class created

--- SenFit/app/src/main/java/com/example/senfit/NetworkManager/NetworkManager.java
+++ SenFit/app/src/main/java/com/example/senfit/NetworkManager/NetworkManager.java
@@ -5,14 +5,28 @@ Author Mitchell Culligan
 Version 1.0
 NetworkManager
 This class is created with the purpose of creating services to interact with the rest api
-specifically made for Sen-fit. This class does so through RetroFit.
+specifically made for Sen-fit. This class does so through RetroFit. This class implements the singleton deisign pattern
  */
 package com.example.senfit.NetworkManager;
 
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class NetworkManager {
     private static final String BASE_URL="https://limitless-woodland-96285.herokuapp.com/";
     private static Retrofit retrofit=null;
+
+
+    public static <T> T  createNetworkService(Class<T> serviceClass){
+        if(retrofit==null){
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)//URL for senfit API
+                    .addConverterFactory(GsonConverterFactory.create())//convert JSON to java objects
+                    .client(new OkHttpClient.Builder().build())
+                    .build();
+        }
+        return retrofit.create(serviceClass);
+    }
 
 }
