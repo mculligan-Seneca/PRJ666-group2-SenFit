69d446a7a443f949e92a2f511043ee8a0a93b966
mculligan-Seneca <44836281+mculligan-Seneca@users.noreply.github.com>
2021-03-30 11:01:06 -0400

    gymclass service added

---
+++ SenFit/app/src/main/java/com/example/senfit/NetworkManager/NetwrokServices/GymClassService.java
@@ -0,0 +1,11 @@
+/*
+PRJ666 Sen-Fit
+init date: March 29th 2021
+Author Mitchell Culligan
+Version 1.0
+This interface repersents a service that interacts with the Sen-FIt API that specializes in dealing with GymClass data.
+ */
+package com.example.senfit.NetworkManager.NetwrokServices;
+
+public interface GymClassService {
+}
