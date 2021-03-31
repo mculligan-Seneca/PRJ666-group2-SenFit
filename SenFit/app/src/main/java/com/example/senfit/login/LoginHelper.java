b63a5aaf8438c21fdffdb51155592ee3de801d73
mculligan-Seneca <44836281+mculligan-Seneca@users.noreply.github.com>
2021-03-30 20:50:49 -0400

    login implemented

--- SenFit/app/src/main/java/com/example/senfit/login/LoginHelper.java
+++ SenFit/app/src/main/java/com/example/senfit/login/LoginHelper.java
@@ -9,11 +9,19 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.graphics.drawable.Icon;
 import android.os.AsyncTask;
+import android.util.Log;
 
+import com.example.senfit.NetworkManager.Interceptor.AuthInterceptor;
+import com.example.senfit.NetworkManager.NetworkManager;
+import com.example.senfit.NetworkManager.NetwrokServices.LoginService;
 import com.example.senfit.dataContext.DatabaseClient;
 import com.example.senfit.dataContext.entities.Member;
 
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 import static android.content.Context.MODE_PRIVATE;
 
@@ -36,7 +44,32 @@ public class LoginHelper {
      */
     public static void compareEmailPass(Context context, String email, String password,
                                         LoginActivity.ComparisonCallback comparisonCallback) {
-        new AsyncTask<Object, Object, Object>() {
+        LoginService loginService = NetworkManager.getNetworkManager().createNetworkService(LoginService.class);
+        Call<Member> memberCall = loginService.login(new LoginBody(email,password));
+        memberCall.enqueue(new Callback<Member>() {
+            @Override
+            public void onResponse(Call<Member> call, Response<Member> response) {
+                    if(!response.isSuccessful()){
+                        comparisonCallback.isValid(false, true);
+                        return;
+                    }
+                    Member member = response.body();
+                    NetworkManager.getNetworkManager().addInterceptorToClient(new AuthInterceptor(member.getToken()));
+                    //retrieve token for member
+                    DatabaseClient.dbExecutors.execute(()->{
+                        DatabaseClient.initDB(context).getAppDatabase()
+                                .getMemberDao()
+                                .insertMember(member);
+                    });
+                comparisonCallback.isValid(true, true);
+            }
+
+            @Override
+            public void onFailure(Call<Member> call, Throwable t) {
+                Log.e("login_api_err",t.getMessage());
+            }
+        });
+        /*new AsyncTask<Object, Object, Object>() {
             @Override
             protected Object doInBackground(Object[] objects) {
                 List<Member> memberList = DatabaseClient.initDB(context)
@@ -59,7 +92,7 @@ public class LoginHelper {
                 comparisonCallback.isValid(isValidUser, isValidPass);
                 return null;
             }
-        }.execute();
+        }.execute();*/
     }
 
     public static void setMemberId(Context context, int memberId) {
