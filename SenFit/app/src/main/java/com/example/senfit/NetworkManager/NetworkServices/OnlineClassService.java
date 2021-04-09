8715c8594919863e7386f0f315aabc1d7431e512
mculligan-Seneca <44836281+mculligan-Seneca@users.noreply.github.com>
2021-04-09 10:45:47 -0400

    online class service added

---
+++ SenFit/app/src/main/java/com/example/senfit/NetworkManager/NetworkServices/OnlineClassService.java
@@ -0,0 +1,13 @@
+/*
+PRJ666 Sen-Fit
+init date: April 9th 2021
+Author Mitchell Culligan
+Version 1.0
+OnlineClassService
+This interface repersents a service that interacts with the Sen-FIt API that specializes in dealing with OnlineClass data.
+ */
+package com.example.senfit.NetworkManager.NetworkServices;
+
+public interface OnlineClassService {
+
+}
