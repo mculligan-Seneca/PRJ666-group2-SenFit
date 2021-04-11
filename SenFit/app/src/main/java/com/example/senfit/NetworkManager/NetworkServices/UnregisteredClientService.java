1816aaec58f1c6c4c441515101ccf4aec5da05be
mculligan-Seneca <44836281+mculligan-Seneca@users.noreply.github.com>
2021-04-11 17:32:43 -0400

    unregisteredclient service

--- SenFit/app/src/main/java/com/example/senfit/NetworkManager/NetworkServices/UnregisteredClientService.java
+++ SenFit/app/src/main/java/com/example/senfit/NetworkManager/NetworkServices/UnregisteredClientService.java
@@ -13,9 +13,10 @@ import com.example.senfit.dataContext.entities.UnregisteredClient;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 public interface UnregisteredClientService {
 
-    @GET("/registerClient")
+    @POST("/registerClient")
     public Call<UnregisteredClient> registerClient(@Body UnregisteredClient client);
 }
